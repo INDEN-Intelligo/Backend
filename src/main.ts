c74add9f8c3801bea7323a713340944712dbd5c1
ghost-hikaru <melvin.mathurin35@gmail.com>
2023-03-09 09:03:32 +0100

    autorize the front to make connection to the back

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  app.enableCors(
+    {'origin':'http://localhost:4200'}
+  );
 }
 bootstrap();
+
+
+
